dc6facc9d2b0488f88e2f90270e9929ebfb8c668
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-12-19 17:28:00 +0200

    Create file

---
+++ 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.py
@@ -0,0 +1,16 @@
+        while curr:
+            if curr:
+                allNodes.append(curr)
+            curr = curr.next
+        if n >= len(allNodes) or n <= 0:
+        allNodes[-n-1].next = allNodes[-n].next
+        curr = head
+        allNodes = []
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+            return head.next
+        return head
+        
+class Solution:
+#         self.next = next
+[
+
