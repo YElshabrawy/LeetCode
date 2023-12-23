533e0800a10d16197dc7ee44cd12b4b60b4f4582
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-12-23 13:42:45 +0200

    Create file

---
+++ 148-SortList/148-SortList.py
@@ -0,0 +1,14 @@
+            return b
+ 
+        # Base cases
+        if a == None:
+    def sortedMerge(self, a, b):
+        result = None
+
+class Solution:
+#         self.val = val
+#         self.next = next
+#     def __init__(self, val=0, next=None):
+# Definition for singly-linked list.
+# class ListNode:
+[
