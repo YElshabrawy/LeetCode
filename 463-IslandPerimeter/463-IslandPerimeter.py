568950ab610848e61637e1e0420c0f99d74b5c94
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-12-23 22:31:37 +0200

    Create file

---
+++ 463-IslandPerimeter/463-IslandPerimeter.py
@@ -0,0 +1,14 @@
+····················cumm·+=·4
+····················if·j-1·>=·0·and·row[j-1]·==·1:
+························cumm·-=·1
+····················if·j+1·<=·len(row)·-·1·and·row[j+1]·==·1:
+························cumm·-=·1
+····················if·i-1·>=·0·and·grid[i-1][j]·==·1:
+························cumm·-=·1
+················if·cell·==·1:
+············for·j,·cell·in·enumerate(row):
+    ····for·i,·row·in·enumerate(grid):
+        cumm = 0
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+class Solution:
+[
