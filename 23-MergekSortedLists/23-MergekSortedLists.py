9218bff5541fdbb1a411d3ecae3d89f75557269a
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-01-03 22:53:46 +0200

    Create file

---
+++ 23-MergekSortedLists/23-MergekSortedLists.py
@@ -0,0 +1,14 @@
+                heapq.heappush(h, (lists[i].val, i))
+                lists[i] = lists[i].next
+            if lists[i]:
+        for i in range(len(lists)):
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        # Create a heap with all heads for each linked list
+        h = []
+#         self.next = next
+class Solution:
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+# Definition for singly-linked list.
+[
